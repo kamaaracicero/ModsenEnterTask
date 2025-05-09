7ebb7a691b750de7ced5b9eef0b18d337063de83
AndreyVerovkin <verovkin02@gmail.com>
2025-05-09 19:23:33 +0300

    add main db context

--- EnterTask.DataAccess/DbContexts/MainDbContext.cs
+++ EnterTask.DataAccess/DbContexts/MainDbContext.cs
@@ -1,8 +1,41 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EnterTask.Data.DataEntities;
+using EnterTask.DataAccess.CustomConverters;
+using EnterTask.DataAccess.EntityConfigurations;
+using Microsoft.EntityFrameworkCore;
 
 namespace EnterTask.DataAccess.DbContexts
 {
     internal class MainDbContext : DbContext
     {
+        public MainDbContext(DbContextOptions<MainDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Event> Events { get; set; } = null!;
+
+        public DbSet<EventChange> EventsChange { get; set; } = null!;
+
+        public DbSet<Participant> Participants { get; set; } = null!;
+
+        public DbSet<Registration> Registrations { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EventConfiguration());
+            modelBuilder.ApplyConfiguration(new EventChangeConfiguration());
+            modelBuilder.ApplyConfiguration(new ParticipantConfiguration());
+            modelBuilder.ApplyConfiguration(new RegistrationConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder
+                .Properties<DateOnly>()
+                .HaveConversion<DateOnlyConverter>()
+                .HaveColumnType("date");
+
+            base.ConfigureConventions(configurationBuilder);
+        }
     }
 }
---
+++ EnterTask.DataAccess/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using EnterTask.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EnterTask.DataAccess
+{
+    internal static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Default");
+
+            services.AddDbContext<MainDbContext>(options => {
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+    }
+}

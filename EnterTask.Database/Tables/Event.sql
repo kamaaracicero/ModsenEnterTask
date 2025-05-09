bbc5c666c7384c4765704d4bb86dbcb8c20af911
AndreyVerovkin <verovkin02@gmail.com>
2025-05-09 16:53:05 +0300

    adding default tables

---
+++ EnterTask.Database/Tables/Event.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Event]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+	[Name] NVARCHAR(300) NOT NULL,
+	[Description] NVARCHAR(2000) NOT NULL,
+	[Start] DATETIME NOT NULL,
+	[Place] NVARCHAR(150) NOT NULL,
+	[Category] NVARCHAR(100) NOT NULL,
+	[MaxPeopleCount] INT NOT NULL,
+	[Picture] NVARCHAR(MAX) NOT NULL,
+
+	CONSTRAINT pk_Event PRIMARY KEY ([Id]),
+)

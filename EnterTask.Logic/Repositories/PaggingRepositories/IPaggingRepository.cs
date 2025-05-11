9275aa3026f8cf742b43e50e97d8215dfe07ee4d
AndreyVerovkin <verovkin02@gmail.com>
2025-05-11 03:44:41 +0300

    forget interface

---
+++ EnterTask.Logic/Repositories/PaggingRepositories/IPaggingRepository.cs
@@ -0,0 +1,13 @@
+﻿using EnterTask.Data.DataEntities;
+using EnterTask.Data.Repository;
+
+namespace EnterTask.Logic.Repositories.PaggingRepositories
+{
+    public interface IPaggingRepository<TEntity>
+        where TEntity : class, IDataEntity, new()
+    {
+        Task<RepositoryResult<IEnumerable<TEntity>>> GetPage(int page, int pageSize);
+
+        Task<RepositoryResult<(int pages, int elements)>> GetTotalCount(int pageSize);
+    }
+}
